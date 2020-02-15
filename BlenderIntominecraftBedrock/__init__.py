1f122bbf9c09f8b14d934993b52fd2e14491a5c1
SkylerPIlot <SkylerParthemore@gmail.com>
2020-02-14 18:42:34 -0800

    Created default addon layout for Blender

---
+++ BlenderIntominecraftBedrock/__init__.py
@@ -0,0 +1,15 @@
+bl_info = {
+    "name": "BlenderintominecraftBedrock",
+    "author": "SkylerPIlot",
+    "version": (0, 1),
+    "blender": (2, 81, ),
+    "location": "",
+    "description": "Export to .json",
+    "wiki_url": "",
+    "tracker_url": "",
+    "support": "",
+    "category": "Import-Export"
+}
+
+if __name__ == "__main__":
+    print("hello")
